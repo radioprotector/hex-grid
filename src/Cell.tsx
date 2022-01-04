61276b9b32ec8c44becfad73cb06637f8547d307
Cate Stock <radioprotector@users.noreply.github.com>
2022-01-04 01:05:20 -0500

    move to function/hooks

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -1,69 +1,70 @@
-import React from 'react';
-import { connect, ConnectedProps } from 'react-redux';
+import { useMemo } from 'react';
 
 import { Hex } from 'honeycomb-grid';
-import { RootState, selectScaledHue, selectScaledSaturation, selectScaledLightness, selectIsCenter } from './store';
+import { selectScaledHue, selectScaledSaturation, selectScaledLightness, selectIsCenter } from './store';
+import { useAppSelector } from './hooks';
 
-interface CellProps extends PropsFromRedux {
-  hex: Hex<any>
-}
+function Cell(props: { hex: Hex<any> }): JSX.Element {
+  const gridDimensions = useAppSelector((state) => state.hexGrid.gridDimensions);
+  const centerHexCoord = useAppSelector((state) => state.hexGrid.centerCoord);
+  const pointString = useAppSelector((state) => state.hexGrid.cellPointsString);
+  const baseHue = useAppSelector((state) => state.color.hue);
+  const baseSaturation = useAppSelector((state) => state.color.saturation);
+  const baseLightness = useAppSelector((state) => state.color.lightness); 
 
-class Cell extends React.Component<CellProps> {
-  render() {
-    const scaledHue = selectScaledHue(this.props.baseHue, this.props.gridDimensions, this.props.hex);
-    const scaledSaturation = selectScaledSaturation(this.props.baseSaturation, this.props.gridDimensions, this.props.hex);
-    const scaledLightness = selectScaledLightness(this.props.baseLightness, this.props.gridDimensions, this.props.hex);
-    const fill = `hsl(${scaledHue}, ${scaledSaturation}%, ${scaledLightness}%)`;
-    let stroke = fill;
+  const scaledHue = useMemo(() => {
+    return selectScaledHue(baseHue, gridDimensions, props.hex)
+  }, [baseHue, gridDimensions, props.hex]);
 
-    // Change the stroke based on whether this is the "central" hex
-    let strokeWidth = 1;
-    let strokeOpacity = 0.15;
+  const scaledSaturation = useMemo(() => {
+    return selectScaledSaturation(baseSaturation, gridDimensions, props.hex)
+  }, [baseSaturation, gridDimensions, props.hex]);
 
-    if (selectIsCenter(this.props.centerHex, this.props.hex)) {
-      strokeWidth = 2;
-      strokeOpacity = 0.5;
-      stroke = 'black';
-    }
+  const scaledLightness = useMemo(() => {
+    return selectScaledLightness(baseLightness, gridDimensions, props.hex)
+  }, [baseLightness, gridDimensions, props.hex]);
 
-    // Use x/y coords to translate the polygon
-    const {x, y} = this.props.hex.toPoint();
-    const transform = `translate(${x}, ${y})`;
+  const scaledColorString = useMemo(() => {
+    return `hsl(${scaledHue}, ${scaledSaturation}%, ${scaledLightness}%)`;
+  }, [scaledHue, scaledSaturation, scaledLightness]);
 
-    return (
-      <g
-        transform={transform}
-      >
-        <polygon
-          points={this.props.pointString}
-          fill={fill}
-          stroke={stroke}
-          strokeOpacity={strokeOpacity}
-          strokeWidth={strokeWidth}
-        />
-        {/* <text
-          fill="#fff"
-          textAnchor="middle"
-          x="60"
-          y="60"
-        >
-          {this.props.hex.q}, {this.props.hex.r}, {this.props.hex.s}
-        </text> */}
-      </g> 
-    );
+  let stroke = scaledColorString;
+
+  // Change the stroke based on whether this is the "central" hex
+  let strokeWidth = 1;
+  let strokeOpacity = 0.15;
+
+  if (selectIsCenter(centerHexCoord, props.hex)) {
+    strokeWidth = 2;
+    strokeOpacity = 0.5;
+    stroke = 'black';
   }
+
+  // Use x/y coords to translate the polygon
+  const {x, y} = props.hex.toPoint();
+  const transform = `translate(${x}, ${y})`;
+
+  return (
+    <g
+      transform={transform}
+    >
+      <polygon
+        points={pointString}
+        fill={scaledColorString}
+        stroke={stroke}
+        strokeOpacity={strokeOpacity}
+        strokeWidth={strokeWidth}
+      />
+      {/* <text
+        fill="#fff"
+        textAnchor="middle"
+        x="60"
+        y="60"
+      >
+        {this.props.hex.q}, {this.props.hex.r}, {this.props.hex.s}
+      </text> */}
+    </g> 
+  );
 }
 
-const mapStateToProps = (state: RootState) => ({
-  pointString: state.hexGrid.cellPointsString,
-  gridDimensions: state.hexGrid.gridDimensions,
-  centerHex: state.hexGrid.centerCoord,
-  baseHue: state.color.hue,
-  baseSaturation: state.color.saturation,
-  baseLightness: state.color.lightness,
-});
-
-// HACK: We don't need dispatch here but passing mapDispatchToProps=null will break the typings generation
-const connector = connect(mapStateToProps);
-type PropsFromRedux = ConnectedProps<typeof connector>
-export default connector(Cell);
+export default Cell;
