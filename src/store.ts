1107045470c7f72d8c2ef831fc1d298e261e631f
Cate Stock <radioprotector@users.noreply.github.com>
2022-01-04 01:29:36 -0500

    fix scaling implementation

--- src/store.ts
+++ src/store.ts
@@ -23,10 +23,21 @@ export type AppDispatch = typeof store.dispatch;
  * @returns The resulting scaled value.
  */
 function scaleNumericValue(fromValue: number, fromRange: [number, number], toRange: [number, number]): number {
-  // Scale JS value https://gist.github.com/fpillet/993002
-  const scalingFactor = (fromRange[1] - fromRange[0]) / (toRange[1] - toRange[0]);
+  const scalingFactor = (toRange[1] - toRange[0]) / (fromRange[1] - fromRange[0]);
   const valueInRange = Math.min(fromRange[1], Math.max(fromRange[0], fromValue)) - fromRange[0];
-  return Math.round(scalingFactor * valueInRange + toRange[0]);
+
+  return toRange[0] + (valueInRange * scalingFactor);
+}
+
+/**
+ * Clamps a numeric value into a specific range.
+ * @param value The value to clamp.
+ * @param minimum The minimum value in the range.
+ * @param maximum The maximum value in the range.
+ * @returns The clamped value.
+ */
+function clamp(value: number, minimum: number, maximum: number) {
+  return Math.max(minimum, Math.min(maximum, value));
 }
 
 /**
@@ -49,10 +60,10 @@ export const selectIsCenter = (centerHex: PointLike, hex: Hex<any>): boolean => 
  */
 export const selectScaledHue = (baseHue: number, gridDimensions: Size, hex: Hex<any>): number => {
   // Q = 0 to the maximum number of columns
-  return scaleNumericValue(
+  return Math.round(scaleNumericValue(
     hex.q,
     [0, gridDimensions.width],
-    [baseHue - 72, baseHue + 72]);
+    [baseHue - 72, baseHue + 72]));
 };
 
 /**
@@ -69,7 +80,7 @@ export const selectScaledSaturation = (baseSaturation: number, gridDimensions: S
     [-gridDimensions.height, gridDimensions.height],
     [baseSaturation - 15, baseSaturation + 15]);
 
-  return Math.max(0, Math.min(100, percentage));
+  return Math.round(clamp(percentage, 0, 100));
 };
 
 /**
@@ -87,5 +98,5 @@ export const selectScaledLightness = (baseLightness: number, gridDimensions: Siz
     [-gridDimensions.height, gridDimensions.height],
     [baseLightness + 15, baseLightness - 15]);
 
-  return Math.max(0, Math.min(100, percentage));
+  return Math.round(clamp(percentage, 0, 100));
 };
